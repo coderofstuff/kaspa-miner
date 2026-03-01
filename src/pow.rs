d7e2e6e8c0dc62c36805730de582a5100968f4fa
coderofstuff <114628839+coderofstuff@users.noreply.github.com>
2026-02-28 21:40:31 -0700

    larger min target

--- src/pow.rs
+++ src/pow.rs
@@ -55,7 +55,7 @@ impl State {
         // The pow hash must be less or equal than the claimed target.
         // println!("self.target | {:#?}", self.target.to_le_bytes());
         let min_target = Uint256::from_le_bytes([
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 16, 0, 0,
         ]);
         pow <= min_target.min(self.target)
     }
